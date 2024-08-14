0788eb7985eb0f8f35236215295b8aa3b071ccd8
Antonio Poloni <antoniopolonijr@gmail.com>
2024-08-14 14:44:50 -0300

    Fix Id values, Implement function to check if the user input is empty and event listener to clear the inputs

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 /// Telephone Number Validator
 
 // variables assigned to access elements in HTML document
-const convertBtn = document.getElementById("convert-btn"); // set the "Convert" buttom
-const clearBtn = document.getElementById("clear-btn"); // set the "Convert" buttom
-const numberInput = document.getElementById("number"); // set the "input"
-const output = document.getElementById("output"); // set the "output"
+const checkBtn = document.getElementById("check-btn"); // set the "Check" buttom
+const clearBtn = document.getElementById("clear-btn"); // set the "Clear" buttom
+const userInput = document.getElementById("user-input"); // set the "input"
+const resultsDiv = document.getElementById("results-div"); // set the "output"
 
-// TODO
-// function to check the User Input
-const checkUserInput = () => {};
+/// Functions
 
-// TODO
-// function to clear Output
-const clearOutput = () => {};
+// function to check if the user input is empty, or it's a Valid format for US phone number
+const checkUserInput = (originalUserInput) => {
+  if (originalUserInput === "") {
+    // if the number input is empty
+    window.alert("Please provide a phone number");
+    return; // to break out of this function early. This will prevent future code in this function from running.
+  }
+
+  // TODO check if the input is valid
+};
 
 // Event Listeners
 
-// event listener to call the clearOutput function when users click the Clear button.
-clearBtnBtn.addEventListener("click", clearOutput);
+// event listener to clear input and output text when users click the Clear button.
+clearBtn.addEventListener("click", () => {
+  userInput.value = ""; // clear the input text in the html
+  resultsDiv.textContent = ""; // clear the output text in the html
+});
 
 // event listener to call the checkUserInput function when users click the Check button.
-convertBtn.addEventListener("click", checkUserInput);
+checkBtn.addEventListener("click", () => {
+  checkUserInput(userInput.value); // take the value of text input and execute the checkUserInput function
+  userInput.value = ""; // clear the input text in the html
+});
 
 // event listener to call the checkUserInput function when users press the Enter / Return key.
-numberInput.addEventListener("keydown", (e) => {
+userInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     e.preventDefault(); // method to stop the browser from refreshing the page after submitting the form.
-    checkUserInput();
+    checkUserInput(userInput.value); // take the value of text input and execute the checkUserInput function
+    userInput.value = ""; // clear the input text in the html
   }
 });
